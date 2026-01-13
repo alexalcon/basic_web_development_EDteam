d899d1554886cc8f7015adbb2f3d4b89670a77dc
alexalcon <alexalcon1596@gmail.com>
2026-01-13 17:53:03 -0400

    JavaScript - Module 5: Add first-class functions demonstration in functions.js

--- javascript/module5/functions.js
+++ javascript/module5/functions.js
@@ -80,3 +80,114 @@ console.log(checkBatteryLevel(75)); // Output: Battery optimal - Full operation 
 // remember this type coersion in JavaScript: string + number = string concatenation
 // thus, if a parameter is expected to be a number, ensure to pass a number type
 // or if it is a number type it will be coerced to string during concatenation
+
+console.log("\n");
+
+/**
+ * ━━━━━━━━━━━━━━━━━━━━━
+ * First Class Functions
+ * ━━━━━━━━━━━━━━━━━━━━━
+ * 
+ * In JavaScript, functions are first-class citizens. This means that functions can be:
+ *
+ *  - Assigned to variable (because fundamental data types are defined as 
+ *    first-class citizens stored in variables).
+ *  - Passed as arguments to other functions.
+ *  - Returned from other functions.
+ * 
+ * This allows for higher-order functions and functional programming techniques.
+ */
+
+console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+console.log("First Class Functions in JavaScript");
+console.log("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
+
+// 1. assigning functions to variables
+const stopRobot = function() {
+    console.log("Robot stopped");
+};
+
+stopRobot(); // output: Robot stopped
+
+// assigning with arrow function
+const rotateRobot = (angle) => {
+    console.log("Robot rotating " + angle + " degrees");
+};
+
+rotateRobot(90); // output: Robot rotating 90 degrees
+
+console.log("\n");
+
+// 2. passing functions as arguments (higher-order functions)
+function executeRobotCommand(command) {
+    console.log("Executing command...");
+    command(); // call the function passed as argument
+    console.log("Command completed");
+}
+
+function pickUpObject() {
+    console.log("Gripper activated - Object picked up");
+}
+
+function releaseObject() {
+    console.log("Gripper released - Object dropped");
+}
+
+executeRobotCommand(pickUpObject);
+console.log("\n");
+executeRobotCommand(releaseObject);
+
+console.log("\n");
+
+// 3. returning functions from other functions
+function createSensorReader(sensor_type) {
+    if (sensor_type === "temperature") {
+        return function() {
+            return "Temperature: " + (20 + Math.random() * 10).toFixed(2) + "°C";
+        };
+    } else if (sensor_type === "distance") {
+        return function() {
+            return "Distance: " + (Math.random() * 100).toFixed(2) + " cm";
+        };
+    }
+}
+
+const tempSensor = createSensorReader("temperature");
+const distSensor = createSensorReader("distance");  
+
+console.log(tempSensor()); // output: Temperature: XX.XX°C
+console.log(distSensor()); // output: Distance: XX.XX cm
+
+console.log("\n");
+
+// 4. storing functions in arrays
+const robotActions = [
+    function() { console.log("Scanning environment"); },
+    function() { console.log("Mapping obstacles"); },
+    function() { console.log("Planning route"); },
+    function() { console.log("Executing navigation"); }
+];
+
+console.log("Robot autonomous sequence:");
+for (let i = 0; i < robotActions.length; i++) {
+    robotActions[i]();
+}
+
+console.log("\n");
+
+// 5. functions in objects (methods)
+const robot = {
+    name: "R2-D2",
+    battery: 85,
+    status: function() {
+        console.log(this.name + " battery: " + this.battery + "%");
+    },
+    charge: function(amount) {
+        this.battery = Math.min(100, this.battery + amount);
+        console.log("Charged " + amount + "%. Current battery: " + this.battery + "%");
+    }
+};
+
+robot.status();  // Output: R2-D2 battery: 85%
+robot.charge(10); // Output: Charged 10%. Current battery: 95%
+robot.status();  // Output: R2-D2 battery: 95%
